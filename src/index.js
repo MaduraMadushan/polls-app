3610bfa1d33358fc288de41ab5d722179dcf8342
MaduraMadushan <45748038+MaduraMadushan@users.noreply.github.com>
2019-03-11 06:13:11 +0530

    Adding Middleware

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers/index";
+import middleware from "./middleware/index";
 import App from "./components/App";
 import "./styles.css";
 
-const store = createStore(reducer);
+const store = createStore(reducer, middleware);
 
 console.log(store.getState());
 
